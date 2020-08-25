79b9df3f5caa06ce08419ed994f3fb9854cb2515
Prajwal Gopal <55605535+Prajwalgopal@users.noreply.github.com>
2020-08-25 11:16:23 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@ mkdir -p /tmp/email
 pip3 install -r requirements.txt
 export FN_AUTH_REDIRECT_URI=http://localhost:8040/google/auth
 export FN_BASE_URI=http://localhost:8040/
-export FN_CLIENT_ID=43626433463-nb7535im2lu0jc6urlh4jmjt0vio6bq4.apps.googleusercontent.com
-export FN_CLIENT_SECRET=QOg-KSYKxBHsbtvST9mdLoa2
+export FN_CLIENT_ID=
+export FN_CLIENT_SECRET=
 
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
-export FN_FLASK_SECRET_KEY=da7MRQjAOs3R9f5qWGnaacJDY6VjTG
+export FN_FLASK_SECRET_KEY=
 
 python3 -m flask run -p 8040
